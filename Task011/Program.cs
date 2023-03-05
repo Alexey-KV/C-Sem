96c4203425a6576bef153946f99b5444597fa3a4
Alexey Kuznetchov <Kuznetchovav@gmail.com>
2023-03-06 01:00:19 +0300

    Added Seminar 2 Tasks up to 012

---
+++ Task011/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа должна вывести случайное трехзначное число 
+// и удалить вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine ($"Ваше случайное число: {number}");
+
+int digit2 = number / 100;
+int digit3 = number % 10;
+
+int result = digit2*10 + digit3;
+
+Console.Write($"Искомое число: {result}");
---
+++ Task012/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет принимать на вход два числа 
+// и выводить, является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите число (A): ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число (B): ");
+int numB = Convert.ToInt32(Console.ReadLine());
+int remainder = 0;
+
+remainder = numA % numB;
+
+if (remainder == 0)
+{
+    Console.Write("Число B кратно числу A");
+}
+else
+{
+    Console.Write($"Число B не кратно числу A, остаток равен: {remainder}");
+}
