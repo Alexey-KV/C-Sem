6ab8506343b999761fc4c96a2b7dc46e7f9c1b30
Alexey Kuznetchov <Kuznetchovav@gmail.com>
2023-03-19 11:30:03 +0300

    Added sem tasks up to Task022

--- Task021/Program.cs
+++ Task021/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Рассчитать расстояние медлу точками в двумерном промтранстве
+// A (3, 6); B (2, 1) -> 5,09, A (7, -5); B (1, -1) -> 7,21
+
+double Distance2D(double ax, double ay, double bx, double by)
+{
+    double distance = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2));
+    return Math.Round(distance, 2);
+}
+
+Console.Write("Введите координату X точки A: ");
+double Ax = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y точки A: ");
+double Ay = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X точки B: ");
+double Bx = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y точки B: ");
+double By = Convert.ToDouble(Console.ReadLine());
+
+double result = Distance2D(Ax, Ay, Bx, By);
+Console.WriteLine(result);
+
+
--- Task022/Program.cs
+++ Task022/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Число N  ->  таблица квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.  2 -> 1, 4.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for( int i = 1; i <= number; i++)//Ex. Цикл через 1: i+=2  <->  i = i + 2
+{
+    System.Console.Write($"{Math.Pow(i, 2)} ");
+}
