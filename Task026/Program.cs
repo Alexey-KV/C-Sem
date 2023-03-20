280a52fc7b468ce500ad31038bb7f828fd6184d9
Alexey Kuznetchov <Kuznetchovav@gmail.com>
2023-03-20 08:11:28 +0300

    Added CShSem Task026

---
+++ Task026/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 26: Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Counter(int number)
+{
+    int counter = 0;
+    while (number > 0)
+    {
+        number/=10;
+        counter++;
+    }
+    return counter;
+}
+
+Console.Write("Enter the number: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Digits number is = {Counter(A)}");
