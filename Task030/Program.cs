3f6b3f1773e06d45990cd6c9e7515e88f870666c
Alexey Kuznetchov <Kuznetchovav@gmail.com>
2023-03-20 09:27:32 +0300

    Added CShSen Task030

---
+++ Task030/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напиште программу, кот выводит массив из 8 эл-тов
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,1,0,1,1]
+
+Console.Write("Введите кол-во эл-тов массиваЖ ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+
+for(int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0,2);
+    Console.Write($"{arr[i]} ");
+}
+
+// "Красивые" методы вывода:
+// Console.Writline('[' + string.Join(", ", arr) + "]");
+// Console.WriteLine($"[{string.Join("; ", arr)}]");
+
+// Random rnd = new Random();
+// соотв
+// вместо arr[i] = rnd.Next(0,2); //см выше
